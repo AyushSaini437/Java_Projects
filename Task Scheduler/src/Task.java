7aab0999dc83aa676a4bab757d3480c6280d1721
AyushSaini437 <ayushsaini437@gmail.com>
2026-02-06 11:59:09 +0530

    Initail commit

---
+++ Task Scheduler/src/Task.java
@@ -0,0 +1,48 @@
+public class Task {
+    int taskID;
+    String taskName;
+    int taskPriority;
+
+    public Task(int taskID, String taskName, int taskPriority) {
+        this.taskID = taskID;
+        this.taskName = taskName;
+        this.taskPriority = taskPriority;
+    }
+
+    public Task(){
+
+    }
+
+    public int getTaskID() {
+        return taskID;
+    }
+
+    public void setTaskID(int taskID) {
+        this.taskID = taskID;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public int getTaskPriority() {
+        return taskPriority;
+    }
+
+    public void setTaskPriority(int taskPriority) {
+        this.taskPriority = taskPriority;
+    }
+
+    @Override
+    public String  toString() {
+        return "Task{" +
+                "taskID=" + taskID +
+                ", taskName='" + taskName + '\'' +
+                ", taskPriority=" + taskPriority +
+                '}';
+    }
+}
